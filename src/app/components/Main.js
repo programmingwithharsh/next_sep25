c8ca978f30eef0f3eea86e13bfe29733a28aef83
Harsh Tiwari <harshtiwari343@email.com>
2025-09-02 12:52:08 +0530

    Added state

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -10,12 +10,26 @@ export default class Main extends React.Component { // Export Main Component, in
     constructor() {  // Class Component lifecycle
         super();
         console.log("Constructor - 1");
+        this.state = {
+            username: "Supriya",
+            address: "Delhi"
+        }
+    }
+
+    updateUsername = () => {
+        this.setState({ // to update state we use this.setState, whenever state updates, component rerender
+            username: "Abhimanyu",
+            address: "Mumbai"
+        })
     }
 
     render() { // Class Component lifecycle
         console.log("Render - 2");
+        console.log(this.state);
         return <div>
             <div>This is Main Class Component 2 + 2 is {2 + 2} </div>
+            <div>Username is {this.state.username}</div>
+            <button onClick={this.updateUsername}>Update Username</button>
             <AddProduct />
             <ProductList />
             <Title />
