9672a842f0e0c9ce763d13f17cf728d1977ea714
Harsh Tiwari <harshtiwari343@email.com>
2025-09-02 11:45:43 +0530

    added separate div

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -6,8 +6,16 @@ import Title from './Title';
 import Welcome from './Welcome';
 
 export default class Main extends React.Component { // Export Main Component, inherit React class
+
+    constructor() {  // Class Component lifecycle
+        super();
+        console.log("Constructor - 1");
+    }
+
     render() { // Class Component lifecycle
-        return <div>This is Main Class Component
+        console.log("Render - 2");
+        return <div>
+            <div>This is Main Class Component 2 + 2 is {2 + 2} </div>
             <AddProduct />
             <ProductList />
             <Title />
